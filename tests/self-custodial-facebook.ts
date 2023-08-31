48e50757f8305a5bf20c09072013985acc4fa5f1
PrakyathReddy <prakyathreddyk@gmail.com>
2023-08-31 15:46:16 +0530

    added tests

--- tests/self-custodial-facebook.ts
+++ tests/self-custodial-facebook.ts
@@ -31,4 +31,37 @@ describe("self-custodial-facebook", () => {
     let userDetails = await program.account.facebookAccount.fetch(userFacebookAddress);
     console.log(`Created a new account with following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`);
   });
+
+  it("Find someone's facebook address", async () => {
+    const userAddress = new anchor.web3.PublicKey("EatHXJpsV6kaq32YNaaERDHntgcG8sCHMKn9oFSg5GtD");
+    const [userFacebookAccountm _] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('self-custodial facebook2'),
+        userAddress.toBuffer(),
+      ]
+      program.programId
+    )
+    try {
+      let userDetails = await program.account.facebookAccount.fetch(userFacebookAccount);
+      console.log(`Created a new account with the following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`);
+    } catch (error) {
+      console.log("User account does not exist :: ", error);
+    }
+  });
+
+  it("Close my facebook account", async() => {
+    const ix = await program.methods.deleteAccount();
+    const userFacebookAddress = (await ix.pubKeys()).facebookAccount;
+    console.log("user facebook address :: ", userFacebookAddress.toString());
+    // create user's facebook address
+    const tx = await ix.rpc();
+    console.log("your transaciton signature", tx)
+    // user details not found since we closed the account
+    try {
+      let userDetails = await program.account.facebookAccount.fetch(userFacebookAddress);
+      console.log(`Created a new account with the following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`);
+    } catch {
+      console.log("User details not found, cuz' we closed it");
+    }
+  });
 });
